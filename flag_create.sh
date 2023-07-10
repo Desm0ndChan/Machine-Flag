ff910829296cfe33399f825ed00d83d5cdb26519
desm0ndchan <heigorphoto@gmail.com>
2023-07-10 21:14:58 +0800

    initial commit

---
+++ flag_create.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# An array of user account names
+user_accounts=("user1" "user2" "user3")
+
+# Machine names
+machine="MACHINE_NAME"
+
+# Create directory for the machine
+mkdir "${machine}"
+
+# Loop to create 10 snapshots for each machine
+for (( i=1; i<=10; i++ ))
+do
+# Create directory SnapshotX
+    mkdir "${machine}/Snapshot${i}"
+
+    # Loop to create 3 flags in each snapshot
+    for user_account in "${user_accounts[@]}"
+    do
+        # Generate flag
+        flag=$(echo ${RANDOM} | md5sum | head -c 25)
+        # Create flag file
+        echo ${flag} > "${machine}/Snapshot${i}/flag_${user_account}.txt"
+    done
+done
