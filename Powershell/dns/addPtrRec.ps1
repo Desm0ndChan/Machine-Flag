671cfe2d88237497c7b4a029f6965093eaf40ed3
desm0ndchan <heigorphoto@gmail.com>
2023-08-10 12:27:52 +0800

    Fixed spelling

--- Powershell/dns/addPtrRec.ps1
+++ Powershell/dns/addPtrRec.ps1
@@ -1,5 +1,5 @@
 $HOSTNAME="HOSTNAME"
 $DOMAIN_NAME="DOMAIN"
 $IP="192.168.1.1"
-Add-DnsServerResourceRecord -Name $HOSTNAME -ZoneName $DOMAIN_NAME -IPv4Address $IP -TimeToLive 01:00:00 -CreatePtr -PassThru
-Get-DnsServerResourceRecord -ZoneName $DOMAIN | Format-Table -AutoSize -Wrap
+Add-DnsServerResourceRecordA -Name $HOSTNAME -ZoneName $DOMAIN_NAME -IPv4Address $IP -TimeToLive 01:00:00 -CreatePtr -PassThru
+Get-DnsServerResourceRecord -ZoneName $DOMAIN_NAME | Format-Table -AutoSize -Wrap
