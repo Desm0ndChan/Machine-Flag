310fb341cc2cd489ef5c0b7eb3d9db67b5dd6fd6
desm0nd chan <heigorphoto@gmail.com>
2023-09-16 15:49:36 +0800

    Fixed naming bug

--- Linux/flag_create.sh
+++ Linux/flag_create.sh
@@ -7,21 +7,21 @@ flag_names=("easy" "intermediate" "hard")
 machine="MACHINE_NAME"
 
 # Create directory for the machine
-mkdir "${machine}"
+mkdir -p "${machine}"
 
 # Loop to create 10 snapshots for each machine
 for (( i=1; i<=10; i++ ))
 do
 # Create directory SnapshotX
-    mkdir "${machine}/Snapshot${i}"
+    mkdir -p "${machine}/Snapshot${i}"
 
     # Loop to create 3 flags in each snapshot
-    for flag_names in "${flag_names[@]}"
+    for flag_name in "${flag_names[@]}"
     do
         # Generate flag
         flag=$(echo ${RANDOM} | md5sum | head -c 25)
         # Create flag file
-        echo Snapshot ${i} flag for ${flag_names} is ${flag}
-        echo ${flag} > "${machine}/Snapshot${i}/flag_${flag_names}.txt"
+        echo Snapshot ${i} flag for ${flag_name} is ${flag}
+        echo ${flag} > "${machine}/Snapshot${i}/flag_${flag_name}.txt"
     done
 done
