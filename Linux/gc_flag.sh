4380fd8676e803dd0ca2597ea0d7f700d3dbd82d
desm0nd chan <heigorphoto@gmail.com>
2023-09-16 23:00:34 +0800

    upgraded version of gc script

--- Linux/gc_flag.sh
+++ Linux/gc_flag.sh
@@ -2,17 +2,17 @@
 
 # An array of user account names
 user_accounts=("user1" "user2" "user3")
-flag_names=("easy","intermediate","hard")
+flag_names=("easy" "intermediate" "hard")
 # Array of directories where to save the flags
 directories=("/path/to/save/flags/user1" "/path/to/save/flags/user2" "/path/to/save/flags/user3")
 
 # Server details
 server="localhost"
-port="8000"
+port="80"
 
-# Machine name and snapshot number taken from command line arguments
-machine="" #change this
-snapshot_number=$2
+# snapshot number taken from command line arguments
+machine="" # Change this
+snapshot_number=$1
 
 # Check if both arguments are provided
 if [ -z "$machine" ] || [ -z "$snapshot_number" ]
@@ -20,6 +20,8 @@ then
   echo "Please provide both machine name and snapshot number"
   exit 1
 fi
+
+declare -A flags
 
 # Loop to retrieve 3 flags for a given snapshot
 for index in "${!user_accounts[@]}"
@@ -30,24 +32,32 @@ do
   # Retrieve flag file using wget and save it to a specific directory, overwriting if it exists
   wget -qO $file_path "http://${server}:${port}/${machine}/Snapshot${snapshot_number}/flag_${flag_names[index]}.txt"
   
+  # Store the flag in the associative array
+  flags["${snapshot_number},${flag_names[index]}"]=$(cat "$file_path")
+
   # Change the permissions of the output file to 400
   chmod 400 $file_path
   # Change the owner and the group of the output file to the user account
   chown ${user_accounts[index]}:${user_accounts[index]} $file_path
-
   # Check if the file exists
   if [ -f "$file_path" ]; then
-    # Print a message with the snapshot number and file name
-    echo "Snapshot ${snapshot_number} file ${file_path} value is:"
     # Print the permissions of the file
-    echo "Permissions: $(ls -la "$file_path")"
-    # Print the contents of the file
-    cat "$file_path"
+    echo "Permission of ${file_path}:"
+    ls -la "$file_path"
     echo ""
   else
     echo "Snapshot ${snapshot_number} file ${file_path} does not exist."
   fi
-
 done
+
+# Print the table
+echo -e "Snapshot\t${flag_names[0]}\t${flag_names[1]}\t${flag_names[2]}"
+row="Snapshot $snapshot_number"
+for flag_name in "${flag_names[@]}"
+do
+    row="${row}\t${flags["$snapshot_number,$flag_name"]}"
+done
+echo -e "$row"
+
 # Run the script as follows: 
-#./gc_flag.sh machine_name snapshot_number
+#./gc_flag.sh snapshot_number
