81d00b4db0033002e3ebadd2dd9c3864ed77f8c4
desm0nd chan <heigorphoto@gmail.com>
2023-10-20 19:02:18 +0800

    Updated automation script

--- Powershell/flag/automate.ps1
+++ Powershell/flag/automate.ps1
@@ -1,11 +1,12 @@
 # Define the IP address (replace with the actual IP)
-$IP = "192.168.1.1" 
-
+$IP = "192.168.2.1" 
+timeout /t 5
 # Download the gc_flag.ps1 file to the Administrator's directory
 Invoke-WebRequest -Uri "http://$IP/gc_flag.ps1" -OutFile "$env:USERPROFILE\gc_flag.ps1"
 
 # Call the gc_flag.ps1 script with an argument
 & "$env:USERPROFILE\gc_flag.ps1" $args[0]
-
+timeout /t 1
 # Remove all .ps1 files from the Administrator's directory
 Remove-Item "$env:USERPROFILE\*.ps1"
+Restart-Computer -force
